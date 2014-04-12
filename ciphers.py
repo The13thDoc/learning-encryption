cf699324274440bb315741f554e036d6bc782217
JH <jonmhenrich@gmail.com>
2014-04-12 05:54:41 -0500

    ROT13 and Caesar cipher working

--- ciphers.py
+++ ciphers.py
@@ -1,10 +1,9 @@
-###
-# Utilize the alphabet. Using ROT13 (where the key is 13),
-# encrypting twice should result in the equivalent of
-# a decrypt.
-###
+#**
+# Caesar cipher.
+#**
 
 import string
+import os
 
 message = "This is a common message.".upper()
 cipherText = ''
@@ -51,11 +50,43 @@ def decrypt(cipherText, key):
 
     return translatedText
 
-print message
-encrypted = encrypt(message, key)
-print encrypted
-print encrypt(encrypted, key)
-print encrypt(encrypt(encrypted, key), key)
+#**
+# ROT13 encryption (where the key is 13).
+# Encrypting twice should result in the equivalent of
+# a decrypt.
+#**
+def rot_13_encryption(text):
+	return encrypt(text, key)
+
+#**
+# ROT13 decryption.
+#**
+def rot_13_decryption(text):
+	return decrypt(text, key)
+
+
+#**
+# Clear the terminal (linux)
+#**
+clear = lambda: os.system('clear')
+
+#=====
+# Main
+#
+# This is to show that ROT13 encryption, which is using a Caesar cipher
+# with a key of 13, decrypts itself using an ecryption. This occurs
+# because 13 is half of 26 (the length of the alphabet).
+#
+# This assumes simple conditions:
+# - Any characters NOT i nthe alphabet are left as is (spaces, dashes, etc.)
+# - All characters will be converted to either uppercase. Lowercase
+# would be fine as well.
+#=====
+clear()
+print 'Trying ROT13 encryption...\n'
+print message, '\t Original message'
+print rot_13_encryption(message), '\t Encrypted'
+print rot_13_encryption(rot_13_encryption(message)), '\t Encrypted again'
 
 
 
