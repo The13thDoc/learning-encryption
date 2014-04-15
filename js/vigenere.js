36ce4d5b1ecb3035cda132a56da4c096ba179575
The13thDoc <thethneedinglorax@gmail.com>
2014-04-15 08:09:47 -0400

    Added vigenere algorithms in Javascript.

--- js/vigenere.js
+++ js/vigenere.js
@@ -2,8 +2,15 @@
  * Vigenere's cipher
  */
 
-var alphabet;
+var alphabet = 'ABCDEFGHIJKLMNAOPQRSTUVWXYZ';
 
+/**
+ * Encryption formula.
+ *
+ * Ci = Ek(Mi) = (Mi + Ki)
+ *
+ * return - Ciphertext character index
+ */
 function E(K, M) {
 	var i = M + K; // Alpha index of resulting character
 
@@ -13,4 +20,61 @@ function E(K, M) {
 	return i;
 }
 
-E;
+/**
+ * Decryption formula.
+ *
+ * Mi = Dk(Ci) = (Ci - Ki)
+ *
+ * return - Plaintext character index
+ */
+function D(K, C){
+	var i = C - K; // Alpha index of resulting character
+
+	if (i < 0) {
+		i = alphabet.length + i;
+	}
+	return i;
+}
+
+/**
+ * Cipher flow.
+ *
+ * initial_text - The message being deciphered.
+ * key - Key used to decipher.
+ * F - Function to encrypt or decrypt
+ */
+function process (initial_text, key, F) {
+	var result_text = '';
+	var key_gen = key;
+
+	for (var i in initial_text) {
+
+		if (key_gen.length === i) {
+
+		}
+
+		if (alphabet.indexOf(i, [0]) !== -1) {
+			var M = alphabet.indexOf(initial_text, [0]);
+			var K = alphabet.indexOf(key_gen, [0]);
+			var C = F(K, M);
+
+			result_text += alphabet.charAt(C);
+		}else{
+			result_text += initial_text.charAt(i);
+		}
+	}
+}
+
+/**
+ * Generate the next key letter from the original.
+ *
+ *  return - Index of the next key character.
+ */
+function get_key_index (key, i) {
+	var next_index = i;
+
+	while (next_index >= key.length) {
+		next_index = next_index - key.length;
+	}
+	return next_index;
+}
